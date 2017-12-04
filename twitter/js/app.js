ab8ba7a2ba5dbd4a235a4626b8efad7992ab1607
brenda <breen.milian@gmail.com>
2017-12-04 10:44:33 -0600

    Subiendo repositorio Retos de Código

---
+++ twitter/js/app.js
@@ -0,0 +1,33 @@
+/*mandar llmar a los elementos
+ var contenedor = document.getElementById('encabezado');
+
+ var seccionTwo = document.getElementById('section-2');
+
+ var seccionThree = document.getElementById('section-3')
+
+ var seccionFour = document.getElementById('section-4');
+
+//Creas los elementos que necesitas (hijos
+
+ var tweetPost = document.createElement("input");
+
+ var tuitear = document.createElement("button");
+
+ var contador = document.createElement("div");
+
+
+//dar atributos
+
+tuitear.style.backgroundColor = "#33CFFF"
+tuitear.style.padding = "6px";
+tuitear.style.display = "in-block";
+
+//agregas eventos
+
+//especificas donde creas los elementos
+
+seccionTwo.appendChild(tweetPost);
+
+seccionTwo.appendChild(tuitear);
+
+seccionTwo.appendChild(contador);/*
